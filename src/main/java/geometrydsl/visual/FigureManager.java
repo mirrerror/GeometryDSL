798c37edb4777d291ee1495daf684a82c07c4419
mirrerror <cayote20@mail.ru>
2024-04-21 15:34:35 +0300

    update figure manager

--- src/main/java/geometrydsl/visual/FigureManager.java
+++ src/main/java/geometrydsl/visual/FigureManager.java
@@ -1,6 +1,9 @@
 package geometrydsl.visual;
 
 import javafx.scene.Group;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
 import javafx.scene.shape.Shape;
 
 public class FigureManager {
@@ -10,7 +13,37 @@ public class FigureManager {
     }
 
     public void removeFigure(Group root, Shape shape) {
+        root.getChildren().remove(shape);
+    }
 
+    public void addLine(Group root, geometrydsl.models.Line line) {
+        Line fxLine = new Line();
+        fxLine.setStartX(line.getX1());
+        fxLine.setStartY(line.getY1());
+        fxLine.setEndX(line.getX2());
+        fxLine.setEndY(line.getY2());
+        fxLine.setStroke(Color.BLACK);
+        addFigure(root, fxLine);
+    }
+
+    public void addCircle(Group root, geometrydsl.models.Circle circle) {
+        Circle fxCircle = new Circle();
+        fxCircle.setCenterX(circle.getX());
+        fxCircle.setCenterY(circle.getY());
+        fxCircle.setRadius(circle.getRadius());
+        fxCircle.setStroke(Color.BLACK);
+        fxCircle.setFill(Color.WHITE);
+        addFigure(root, fxCircle);
+    }
+
+    public void addPoint(Group root, geometrydsl.models.Point point) {
+        Circle circle = new Circle();
+        circle.setCenterX(point.getX());
+        circle.setCenterY(point.getY());
+        circle.setRadius(1);
+        circle.setStroke(Color.BLACK);
+        circle.setFill(Color.BLACK);
+        addFigure(root, circle);
     }
 
 }
--- src/main/java/geometrydsl/visual/JavaFXApplication.java
+++ src/main/java/geometrydsl/visual/JavaFXApplication.java
@@ -14,16 +14,12 @@ public class JavaFXApplication extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Group root = new Group();
-        Scene scene = new Scene(root,600,600, Color.BLACK);
+        Scene scene = new Scene(root,600,600, Color.WHITE);
         Stage stage = new Stage();
-        Line line = new Line();
-        line.setStartX(50);
-        line.setStartY(50);
-        line.setEndX(100);
-        line.setEndY(100);
-        line.setStroke(Color.RED);
-        //line.setStroke(5);
-        figureManager.addFigure(root,line );
+
+        figureManager.addLine(root, new geometrydsl.models.Line("test", 10, 10, 100, 100));
+        figureManager.addCircle(root, new geometrydsl.models.Circle("test1", 200, 200, 50));
+        figureManager.addPoint(root, new geometrydsl.models.Point("test2", 300, 300));
 
         stage.setScene(scene);
         stage.show();
