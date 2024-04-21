c713f1244dcd4380534d90f2762b0db7027662bf
UrMjsty <alexciornyi@gmail.com>
2024-04-21 15:29:56 +0300

    add figuremanager implementation

--- src/main/java/geometrydsl/visual/FigureManager.java
+++ src/main/java/geometrydsl/visual/FigureManager.java
@@ -6,7 +6,7 @@ import javafx.scene.shape.Shape;
 public class FigureManager {
 
     public void addFigure(Group root, Shape shape) {
-
+        root.getChildren().add(shape);
     }
 
     public void removeFigure(Group root, Shape shape) {
--- src/main/java/geometrydsl/visual/JavaFXApplication.java
+++ src/main/java/geometrydsl/visual/JavaFXApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 
 public class JavaFXApplication extends Application {
@@ -11,9 +12,18 @@ public class JavaFXApplication extends Application {
     private final FigureManager figureManager = new FigureManager();
 
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage primaryStage) throws Exception {
         Group root = new Group();
-        Scene scene = new Scene(root, Color.BLACK);
+        Scene scene = new Scene(root,600,600, Color.BLACK);
+        Stage stage = new Stage();
+        Line line = new Line();
+        line.setStartX(50);
+        line.setStartY(50);
+        line.setEndX(100);
+        line.setEndY(100);
+        line.setStroke(Color.RED);
+        //line.setStroke(5);
+        figureManager.addFigure(root,line );
 
         stage.setScene(scene);
         stage.show();
